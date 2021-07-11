c4c492a94d7be7d58800411477819b0ba74fbbd4
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-11 16:09:21 -0400

    added posts_page.dart and posts_list.dart

---
+++ lib/posts/view/posts_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PostsList extends StatefulWidget {
+  const PostsList({ Key? key }) : super(key: key);
+
+  @override
+  _PostsListState createState() => _PostsListState();
+}
+
+class _PostsListState extends State<PostsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
