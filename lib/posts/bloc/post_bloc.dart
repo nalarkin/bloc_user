1e27b6121582e6e24e76d121f6c4a7504a3d726a
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-11 15:26:08 -0400

    initialized post bloc

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -8,12 +8,26 @@ part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial());
+  PostBloc({required PostsRepository postsRepository})
+      : _postsRepository = postsRepository,
+        super(
+          PostInitial(),
+        ) {
+    _postSubscription = _postsRepository.posts().listen(_onPostChanged);
+  }
+  final PostsRepository _postsRepository;
+  late final StreamSubscription<List<FirestorePost>> _postSubscription;
 
   @override
-  Stream<PostState> mapEventToState(
-    PostEvent event,
-  ) async* {
+  Stream<PostState> mapEventToState(PostEvent event) async* {
     // TODO: implement mapEventToState
+  }
+
+  void _onPostChanged(List<FirestorePost> posts) => add(PostAdded());
+
+  @override
+  Future<void> close() {
+    _postSubscription.cancel();
+    return super.close();
   }
 }
